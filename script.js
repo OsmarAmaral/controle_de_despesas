9a0d70fb32db80c20c642296d8d2defbf1b0a2d5
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-11-01 12:03:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
+const transactionUl = document.querySelector("#transactions");
+//console.log(transactionUl);
+
 const dummyTransactions = [
-    {id: 1, name: 'Bolo de Brigadeiro', amount: -20},
-    {id: 2, name: 'Salário', amount: 300},
-    {id: 3, name: 'Torta de Brigadeiro', amount: -10},
-    {id: 4, name: 'Violão', amount: 150}
-]
+  { id: 1, name: "Bolo de brigadeiro", amount: -20 },
+  { id: 2, name: "Salário", amount: 300 },
+  { id: 3, name: "Torta de frango", amount: -10 },
+  { id: 4, name: "Violão", amount: 150 },
+];
 
-//console.log(dummyTransactions)
+const addTransactionIntoDOM = (transaction) => {
+  const operator = transaction.amount < 0 ? "-" : "+";
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
+  const amountWithoutOperator = Math.abs(transaction.amount);
+  const li = document.createElement("li");
 
-const addTransactionsIntoDOM = transaction => {
-    const operator = transaction.amount < 0 ? '-' : '+';
-    const CSSClass = transaction.amount < 0 ? "minus" : "plus";
-    const li = document.createElement("li");
+  li.classList.add(CSSClass);
+  li.innerHTML = `
+        ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+  `;
+  // transactionUl.append(li);
+  transactionUl.prepend(li);
 
-    li.classList.add(CSSClass);
-    li.innerHTML = `${transaction.name} <span> ${operator} R$ ${transaction.amount}</span>`;
-    console.log(li);
-    //console.log(operator);
+  // console.log(li);
+  //console.log(operator);
 
-     {
+  {
     // <li class="minus">
-    //       Salário <span>-$400</span><button class="delete-btn">x</button>
-    //     </li>
-     }
+    //     Salário <span>-$400</span><button class="delete-btn">x</button>
+    // </li>
+  }
 };
 
-console.log(dummyTransactions[0]);
+// addTransactionIntoDOM(dummyTransactions[0]);
+// addTransactionIntoDOM(dummyTransactions[1]);
+
+const updateBalanceValues = () => {
+  const transactionsAmounts = dummyTransactions.map(
+    (transaction) => transaction.amount
+  );
+  console.log(transactionsAmounts);
+};
+
+const init = () => {
+  dummyTransactions.forEach(addTransactionIntoDOM);
+  updateBalanceValues();
+};
+
+init();
